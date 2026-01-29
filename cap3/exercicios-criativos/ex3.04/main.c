980f6f43e941ce5077ccfc31b844c3f5c44cc301
Gustavo Henrique <gustavo.tin2@gmail.com>
2026-01-29 19:11:22 -0300

    exercicios criativos 3.1 a 3.11 concluidos

---
+++ cap3/exercicios-criativos/ex3.04/main.c
@@ -0,0 +1,69 @@
+/**
+ * @file main.c
+ * @author Prof. Dr. David Buzatto
+ * @brief Modelo para desenvolvimento de exercícios criativos usando a engine
+ * de jogos Raylib (https://www.raylib.com/).
+ * 
+ * @copyright Copyright (c) 2025
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <raylib.h>
+
+int main( void ) {
+
+    /*-----------------------------------------------------
+     * A lógica inicial do seu programa deve vir aqui:
+     *     - declaração de variáveis;
+     *     - entrada de dados;
+     *     - processamentos adicionais.
+     ----------------------------------------------------*/
+    
+    
+    // ativa a suavização (antialiasing)
+    SetConfigFlags( FLAG_MSAA_4X_HINT );
+
+    // cria uma janela de 800 pixels de largura por 600 de altura
+    InitWindow( 400, 400, "Exercício 3.4" );
+
+    // configura a quantidade de quatros por segundo da engine
+    SetTargetFPS( 60 );    
+
+    // enquanto não é sinalizado que a janela deve ser fechada
+    while ( !WindowShouldClose() ) {
+
+        // inicia o processo de desenho
+        BeginDrawing();
+
+        // limpa a tela usando uma cor
+        ClearBackground( WHITE );
+
+        /*----------------------------------------------------------------------
+         * A lógica do seu desenho deve vir aqui.
+         ---------------------------------------------------------------------*/
+
+        for (int x = 0; x < 400; x += 20) {
+            for (int y = 0; y < 400; y += 20){
+                if ((x + y) % 40 == 0) {
+                    DrawRectangle(x, y, 20, 20, PURPLE);
+                }
+            }
+        }
+
+        /*----------------------------------------------------------------------
+         * A lógica do seu desenho deve terminar na linha acima.
+         ---------------------------------------------------------------------*/
+
+        // termina o desenho
+        EndDrawing();
+
+    }
+
+    // fecha a janela
+    CloseWindow();
+    return 0;
+
+}
